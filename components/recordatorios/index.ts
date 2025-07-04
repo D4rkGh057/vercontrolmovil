ae69c8b0e8a449fa8f3d006940587360604cfda3
D4rkgh057 <danielfuelpaz@outlook.com>
2025-07-03 23:23:31 -0500

    feat: agregar exportaciones de componentes de recordatorios en index.ts

---
+++ components/recordatorios/index.ts
@@ -0,0 +1,4 @@
+export { RecordatorioCard } from './RecordatorioCard';
+export { RecordatorioFilters } from './RecordatorioFilters';
+export { EmptyRecordatorios } from './EmptyRecordatorios';
+export { AddRecordatorioModal, AddRecordatorioModalComponent } from './AddRecordatorioModal';
